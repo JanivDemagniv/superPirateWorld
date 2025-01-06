da070e695fe0a6a21fa301247953408fd0ada4a8
JanivDemagniv <janivjry@gmail.com>
2025-01-06 15:46:43 +0200

    first commit

---
+++ code/setting.py
@@ -0,0 +1,18 @@
+import pygame, sys
+from pygame.math import Vector2 as vector
+
+WINDOW_WIDTH,WINDOW_HIEGHT = 1280,720
+TILE_SIZE = 64
+ANIMATION_SPEED = 6
+
+#layers
+Z_LAYERS = {
+    'bg': 0,
+    'clouds': 1,
+    'bg title': 2,
+    'path': 3,
+    'bg details': 4,
+    'main': 5,
+    'water': 6,
+    'fg': 7
+}
